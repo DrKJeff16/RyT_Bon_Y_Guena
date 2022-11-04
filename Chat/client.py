24b9589dbb9e10ae0030589601e586c34200eb4e
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-03 18:21:16 -0600

    Progress!!!!

--- Chat/client.py
+++ Chat/client.py
@@ -10,79 +10,68 @@ from time import sleep
 
 HOST = '127.0.0.1'
 ENC = 'utf-8'
+BUF_S = 1024
 
 
 def b(*words, sep=' ', enc=ENC) -> bytes:
     """Convierte una o muchas cadenas a un objeto `bytes` ya codificado."""
-    w_s = sep.join(words)
-    return bytes(w_s, encoding=enc)
+    return bytes(sep.join(words), encoding=enc)
 
 
-def main(port: int) -> int:
+def main(port_n: int) -> int:
     """Función principal"""
-    ret = 0
-
     sck = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-    sck.connect((HOST, port))
-    sleep(5)
+    sck.connect((HOST, port_n))
+    sleep(3)
+    imsg, omsg = list(), list()
 
-    try:
-        msg = sck.recv(1024)
-        print(msg.decode(ENC))
-        sck.send(bytes("Client: Thank you, I'm your client number 1",
-                       encoding=ENC))
+    imsg.append(sck.recv(BUF_S).decode(ENC))
+    print(imsg[-1])
 
-        msg1 = sck.recv(1024)
-        print(msg1.decode(ENC))
+    sleep(1)
 
-    except KeyboardInterrupt:
-        ret = -1
-        print('ABORTING...',
-              file=sys.stderr)
+    omsg.append("Client: Thank you, I'm your client number 1")
+    sck.send(b(omsg[-1]))
+    sleep(1)
 
-        sck.close()
-        sleep(3)
-
-    except Exception:
-        ret = 1
-        print("SOMETHING WENT WRONG...",
-              file=sys.stderr)
-
-        sck.close()
-        sleep(3)
+    imsg.append(sck.recv(BUF_S).decode())
+    print(imsg[-1])
 
     sck.close()
-
-    return ret
+    return 0
 
 
 if __name__ == '__main__':
-    port = 0
+    port_n = 0
 
-    while port <= 0:
+    while port_n <= 0:
         try:
             print('Seleccione el puerto:', end=' ')
-            port = int(input().strip().lstrip('0'))
+            port_n = int(input().strip().lstrip('0'))
 
         except KeyboardInterrupt:
             print('ABORTING...',
+                  end='\n\n',
                   file=sys.stderr)
-
-            sleep(3)
+            sleep(2)
             sys.exit(-1)
 
         except ValueError:
-            print(f'Invalid Port value "{port}"',
+            print(f'Invalid Port value "{port_n}".',
+                  'Try again.',
+                  sep='\n',
+                  end='\n\n',
                   file=sys.stderr)
-
-            sleep(3)
+            sleep(2)
             continue
 
         except Exception:
-            print('Something went wrong. Try again...',
+            print('Something went wrong.',
+                  'Try again.',
+                  sep='\n',
+                  end='\n\n',
                   file=sys.stderr)
-
-            sleep(3)
+            sleep(2)
             continue
 
-    sys.exit(main(port))
+    sys.exit(main(port_n))
--- Chat/server.py
+++ Chat/server.py
@@ -8,93 +8,148 @@ import socket as sock
 import sys
 # import threading as thr
 from time import sleep
+from typing import List, NoReturn, Tuple
 
 HOST = '127.0.0.1'
+PORT = 6060
 ENC = 'utf-8'
+BUF_S = 1024
+MAX_CLIENTS = 10
 
 
 def b(*words, sep=' ', enc=ENC) -> bytes:
     """Convierte una o muchas cadenas a un objeto `bytes` ya codificado."""
-    w_s = sep.join(words)
-    return bytes(w_s, encoding=enc)
+    return bytes(sep.join(words), encoding=enc)
+
+
+class Server:
+    """Server class."""
+
+    def __init__(self, host: str, port: int, str_id='SERVER', backlog=None):
+        """Método Constructor."""
+        self.hp_tup = (host, port)
+        self.host, self.port = self.hp_tup
+        self.id = str_id
+
+        self.backlog = backlog
+
+        self.binded = False
+        self.listening = False
+
+        self.all_clients = set()
+        self.active_clients = set()
+
+        self.start()
+
+    def _start_cond(self) -> List[bool, bool]:
+        return [self.binded, self.listening]
+
+    def start(self) -> NoReturn:
+        """Inicializa el servidor con los sockets."""
+        if self._start_cond() == [False, False]:
+            self.sock = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+            self.sock.bind(self.hp_tup)
+            self.binded = True
+
+            self.listen()
+
+        elif self._start_cond() == [True, True]:
+            pass
+
+        elif self._start_cond() == [True, False]:
+            self.listen()
+
+    def listen(self) -> NoReturn:
+        """Start Listening"""
+        if self._start_cond() == [True, False]:
+            if self.backlog is None:
+                self.sock.listen()
+            else:
+                self.sock.listen(self.backlog)
+
+            self.listening = True
+
+        elif self._start_cond() == [True, True]:
+            pass
+
+        elif self._start_cond() == [False, False]:
+            self.start()
+
+        else:
+            sys.exit(1)
+
+    def close(self, t=30) -> NoReturn:
+        if t in [int, float]:
+            t = abs(int(t))
+
+            if t == 0 or t >= 1800:
+                t = 30
+
+        elif t not in [int]:
+            t = 30
+
+        for c in self.active_clients:
+            c.send(b("[{SERVIDOR}]: El servidor se cerrará en {t} segundos."))
+
+        sleep(t)
+
+        for c in self.active_clients:
+            c.close()
+
+        self.sock.close()
+        self.binded = False
+        self.listening = False
+
+
+class Client:
+    """Client class."""
+
+    def __init__(self, srv: sock.socket):
+        """Constructor Method."""
+        self.sock, self.addr = srv.accept()
+        self.connected = True
+
+        self.imsg = list()
+        self.omsg = list()
+
+    def serv_send(self, *args: Tuple[str]) -> NoReturn:
+        """Send a message to client from server."""
+        pass
 
 
 def main(port: int) -> int:
     """Función principal."""
-    ret = 0
-
     srv = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
     srv.bind((HOST, port))
     srv.listen()
 
-    try:
-        while True:
-            try:
-                csocket, address = srv.accept()
-                print("Connection is established:", address)
+    imsg, omsg, all_omsg = list(), list(), list()
+    n_cli = 1
 
-                omsg = b("Server: You are connected.")
-                csocket.send(omsg)
+    while n_cli <= MAX_CLIENTS:
+        client, address = srv.accept()
+        nick = f'user{n_cli}@{address}'
+        print("Connection is established:",
+              f'"{nick}"')
 
-                imsg = csocket.recv(1024)
-                print(imsg.decode(ENC))
-                sleep(12)
-                omsg = b("Bye!")
-                csocket.send(omsg)
+        omsg.append(f'Server: "{nick}@{address}", you are connected.')
+        client.send(b(omsg[-1]))
 
-            except Exception:
-                ret = 2
+        imsg.append(client.recv(BUF_S).decode(ENC))
+        print(imsg[-1])
 
-            finally:
-                csocket.close()
+        # for i in range(9, 1, -1):
+        #     print(i)
 
-    except KeyboardInterrupt:
-        csocket.close()
-        ret = -1
-        print("ABORTING...",
-              file=sys.stderr)
-        sleep(3)
+        omsg.append("Logging you off. Bye!")
+        client.send(b(omsg[-1]))
 
-    except Exception:
-        ret = 2
-        print("SOMETHING WENT WRONG...",
-              file=sys.stderr)
-        sleep(3)
+        client.close()
 
-    finally:
-        srv.close()
-        return ret
+    srv.sendall(b("Server shutting down in 5 seconds.\nBye!"))
+    srv.close()
+    return 0
 
 
 if __name__ == '__main__':
-    port = 0
-
-    while port <= 0:
-        try:
-            port = int(input("Select port: ").strip().lstrip('0'))
-
-            if port <= 0:
-                raise ValueError("")
-
-        except KeyboardInterrupt:
-            print('ABORTING . . .',
-                  file=sys.stderr)
-
-            sleep(3)
-            sys.exit(-1)
-
-        except ValueError:
-            print('Invalid Port value, cannot be less than 1!',
-                  file=sys.stderr)
-
-            sleep(3)
-            continue
-
-        except Exception:
-            print('Something went wrong. Try again...',
-                  file=sys.stderr)
-
-            sleep(3)
-            continue
-
-    sys.exit(main(port))
+    sys.exit(main(PORT))
