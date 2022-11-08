2dfe0c3290defd5c6c87a4a995c093803c9fc7b5
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-07 19:50:01 -0600

    Modificado: Integrando VG's y Comentarios
    Nota: Las variables globales son `TODAS_MAYUS` y no se deben cambiar.

--- Chat/server.py
+++ Chat/server.py
@@ -6,9 +6,16 @@ HOST = '127.0.0.1'              # Host del servidor
 PORT = 4628                     # Puerto por defecto
 BUF_S = 1024                    # Tamaño del buffer
 
+
+# Crea una variable `sock`, de la clase `socket` con dos
+# Parámetros especiales
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 4000))
-sock.listen(1)
+
+# Aloja el servidor en el HOST:PORT
+sock.bind((HOST, PORT))
+
+# Activa el servidor para que "escuche" otras conexiones
+sock.listen()
 
 try:
     while True:
@@ -17,10 +24,9 @@ try:
         clientsocket.send(bytes("Server: You are connected", encoding='utf-8'))
         msg = clientsocket.recv(1024)
         print(msg.decode("utf-8"))
-        #time.sleep(120)
-        #clientsocket.send(bytes("bye"))
+        time.sleep(120)
+        clientsocket.send(bytes("bye"))
         clientsocket.close()
 except KeyboardInterrupt:
     print ("Finishing . . .")
-    
 sock.close()
