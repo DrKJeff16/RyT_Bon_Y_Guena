f339edbd867cf45e0ec05ca34fa6e27b01fe2602
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-07 19:48:59 -0600

    Nuevo: Variables Globales en Servidor.

--- Chat/server.py
+++ Chat/server.py
@@ -1,6 +1,11 @@
 import socket
 import time
 
+# VARIABLES GLOBALES
+HOST = '127.0.0.1'              # Host del servidor
+PORT = 4628                     # Puerto por defecto
+BUF_S = 1024                    # Tamaño del buffer
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 4000))
 sock.listen(1)
