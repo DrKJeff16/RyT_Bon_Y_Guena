0c41ad9fc7aa6da0636d846c88a69fa8527124bb
Michel Paola Osornio Torres, Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-15 13:32:02 -0600

    Agregado: Base del programa para `Capa 2`.

---
+++ Capa_2/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Ejercicio de Raw Sockets y uso de Capa 2 (`L2`)."""
+import sys
+import socket as sock
+
+
+if __name__ == '__main__':
+    sys.exit(0)
