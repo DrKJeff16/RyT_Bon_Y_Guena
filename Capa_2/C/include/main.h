edd4e5db86b157589ec2b475f77a3c4c2ed44c46
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-16 01:44:48 -0600

    Config: LSP Tweaks.

--- Capa_2/C/include/main.h
+++ Capa_2/C/include/main.h
@@ -35,17 +35,56 @@
 
 // TODO: Entender que es esto.
 typedef struct IpHeader {
-  uch iph_ihl : 5, iph_ver : 4;
-  uch iph_tos;
-  ush iph_len;
-  ush iph_ident;
-  uch iph_flag;
-  ush iph_offset;
-  uch iph_ttl;
-  uch iph_protocol;
-  ush iph_chksum;
-  ui iph_sourceip;
-  ui iph_destip;
-} ip_head;
+	uch iph_ihl:5, iph_ver:4;
+	uch iph_tos;
+	ush iph_len;
+	ush iph_ident;
+	uch iph_flag;
+	ush iph_offset;
+	uch iph_ttl;
+	uch iph_protocol;
+	ush iph_chksum;
+	ui iph_sourceip;
+	ui iph_destip;
+} ip_head_t;
+
+// TODO: Entender que es esto.
+typedef struct UdpHeader {
+	ush udph_srcport;
+	ush udph_destport;
+	ush udph_len;
+	ush udph_chksum;
+} udp_head_t;
+
+// TODO: Entender que es esto.
+typedef struct TcpHeader {
+	ush tcph_srcport;
+	ush tcph_destport;
+	ui tcph_seqnum;
+	ui tcph_acknum;
+	uch tcph_reserved:4, tcph_offset:4;
+	// uch tcph_flags;
+
+	ui tcp_res1:4,      /*little-endian*/
+		tcph_hlen:4,     /*length of tcp header in 32-bit words*/
+		tcph_fin:1,      /*Finish flag "fin"*/
+		tcph_syn:1,       /*Synchronize sequence numbers to start a connection*/
+		tcph_rst:1,      /*Reset flag */
+		tcph_psh:1,      /*Push, sends data to the application*/
+		tcph_ack:1,      /*acknowledge*/
+		tcph_urg:1,      /*urgent pointer*/
+		tcph_res2:2;
+
+	ush tcph_win;
+	ush tcph_chksum;
+	ush tcph_urgptr;
+} tcp_head_t;
+
+typedef struct sockaddr_in sa_in;
+
+static int ONE = 1;
+static const int *VAL = &ONE;
+
+ush csum(ush *buf, int nwords);
 
 #endif // MAIN_H
