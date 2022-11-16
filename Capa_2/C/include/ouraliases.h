abb5e480cabdc57b86f3cd36bbd41af0507384ba
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-15 22:43:26 -0600

    Cambios en estructura.

---
+++ Capa_2/C/include/ouraliases.h
@@ -0,0 +1,18 @@
+# ifndef OURALIASES_H
+# define OURALIASES_H
+
+// Ninguno es necesario realmente, hasta ahora...
+# include <stdint.h>
+# include <stdbool.h>
+# include <stddef.h>
+
+// TODO: Usar `typedef ...` ó `#define ...`?
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef unsigned int ui;
+typedef ui ul; // TODO: Explicar esta blasfemia.
+typedef unsigned char uch;
+typedef unsigned short ush;
+
+# endif // OURALIASES_H
