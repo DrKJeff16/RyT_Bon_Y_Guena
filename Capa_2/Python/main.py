74604dcaccbd5e99d4bc8f693ac6c36ec89a582a
Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-16 22:44:11 -0600

    Movido contenido de Python a su contenedor.

---
+++ Capa_2/Python/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Ejercicio de Raw Sockets y uso de Capa 2 (`L2`)."""
+import sys
+import socket as sock
+
+
+def main() -> int:
+    """Función de Ejecución principal."""
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
