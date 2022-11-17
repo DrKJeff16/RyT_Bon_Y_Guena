e06dad121392ce4d848dd913acc51b5a903595ed
Michel Paola Osornio Torres, Guennadi Maximov Cortés <g.maxc.fox@protonmail.com>
2022-11-17 13:36:07 -0600

    Added: Base main function for Raw UDP.

--- Capa_2/C/raw_udp.c
+++ Capa_2/C/raw_udp.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+	if (argc != 5) {
+		printf("Missing arguments!\n");
+		return 1;
+	}
+
+	char *buffer = (char*) malloc(sizeof(char) * PCKT_LEN);
+	ip_head_t *ip = (ip_head_t *) buffer;
+	udp_head_t *udp = (udp_head_t *) (buffer + sizeof(ip_head_t));
+	sockaddr_IN sin, din;
+	memset(buffer, 0, PCKT_LEN);
+
+	int sd = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
+
+	if (sd < 0) {
+		perror("socket() error.");
+		return 1;
+	}
+
+	printf("socket() - Using SOCK_RAW socket. UDP protocol is OK.\n");
+	sin.sin_family = AF_INET;
+	din.sin_family = AF_INET;
+
+	// Port numbers
+	sin.sin_port = htons(atoi(argv[2]));
+	din.sin_port = htons(atoi(argv[4]));
+
+	// IP addresses
+	sin.sin_addr.s_addr = inet_addr(argv[1]);
+	din.sin_addr.s_addr = inet_addr(argv[3]);
+
+	// FALTA MUCHO...
+
 	return 0;
 }
 
